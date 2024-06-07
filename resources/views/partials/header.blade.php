eb77458326518c4e00b25c6e0b02878c94b13eca
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-06-07 18:39:19 +0200

    Add: header + page donna

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<!-- HEADER -->
+<header>
+
+    <!-- LISTA -->
+    <nav class="header">
+        <div class="container d-flex justify-content-between gap-3">
+        <ul class="d-flex justify-content-center gap-3">
+            <li> <a class="white" href="">Donna </a></li>
+            <li> <a class="white" href="">Uomo </a></li>
+            <li> <a class="white" href="">Bambino </a></li>
+        </ul>
+        <!-- LISTA -->
+
+        <!-- LOGO -->
+        <div class="logo-boolean">
+            <img src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo Boolando">
+        </div>
+        <!-- LOGO -->
+
+        <!-- ICONE -->
+        <div class="">
+            <ul class="d-flex justify-content-center gap-3">
+                <li><a class="white" href=""><i class="fa-regular fa-user"></i></a>icona user</li>
+                <li><a class="white" href=""><i class="fa-regular fa-heart"></i></a>cuore</li>
+                <li><a class="white" href=""><i class="fa-solid fa-bag-shopping"></i></a>carrello</li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+</header>
+<!-- /HEADER -->
